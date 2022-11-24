65878aa066888ab2565b2aa25987ea9370654968
gheryyan <115127443+gheryyan@users.noreply.github.com>
2022-11-24 17:03:14 +0700

    Create variabel.java

---
+++ variabel.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+		String nama = "Gheryyan Washesya Syagara";
+		Integer tahun = 2004;
+
+		System.out.println("Assalamualaikum");
+		System.out.println("Nama saya : "+ nama);
+		System.out.println("Saya Lahir di Tahun : "+ tahun);
+		System.out.println("Sekian terimakasih");
+	}
+}
